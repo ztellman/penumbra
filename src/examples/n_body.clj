96f304957c5fd4035ab6d5127fbdd0d9fdcb26a9
prospero <ztellman@gmail.com>
2009-09-27 02:38:43 -0700

    update n-body example

--- src/examples/n_body.clj
+++ src/examples/n_body.clj
@@ -7,13 +7,26 @@
 ;   You must not remove this notice, or any other, from this software.
 
 (ns examples.n-body
-  (:use [penumbra slate compute]))
+  (:use [penumbra slate compute])
+  (:use [clojure.contrib.seq-utils :only (partition-all)]))
 
 ;NOT COMPLETE
 
-(def s (create-slate))
+(defn gen [min max]
+  (float (+ min (* (rand) (- max min)))))
 
-(with-slate s
+(defn gen-mass [num min max]
+  (wrap (take num (repeatedly #(gen min max))) 1))
+
+(defn gen-velocity [num min max]
+  (wrap (take (* 3 num) (repeatedly #(gen min max))) 3))
+
+(defn gen-position [num min max]
+  (wrap (take (* 3 num) (repeatedly #(gen min max))) 3))
+
+(def slate (create-slate))
+
+(with-slate slate
   (defmap add
     (+ %1 (* k %2)))
 
@@ -30,19 +43,41 @@
     (let [m2 (lookup %1 idx)
           p1 %2, p2 (lookup %2 idx)
           diff (- p1 p2)]
-      (? (= :index (float idx))
+      (if (= :index (float idx))
         (float3 0.0)
         (* (normalize diff)
            (/ (* g m2) (dot diff diff))))))
 
-  (let [num 50
-        m (take num (repeatedly #(float (* (rand) 1.0))))
-        p (map float (take (* 3 num) (repeatedly #(float (* (rand) 1.0)))))
-        v (map float (take (* 3 num) (repeat (float 0.0))))
-        m (wrap m 1)
-        p (wrap p 3)
-        v (wrap v 3)]
-    (gravity {:g 6.673e-11 :idx 0} [m p])
-    (kinetic-energy [m v])
-    (potential-energy {:idx 0} [m p])
-    (add {:k 0.5} [p v])))
+  (defreduce sum (+ %1 %2)))
+
+(defn chunked-add [f chunk-size coll-size]
+  (let [s (partition-all chunk-size (range coll-size))
+        add #(add {:k 1.0} [%1 %2])]
+    (reduce add (map #(reduce add (map f %)) s))))
+
+(defn prn-tex [t]
+  (acquire! t)
+  (println (with-out-str (prn (unwrap* t)))))
+
+(defn run-sim [num iterations]
+  (let [m (gen-mass num 10 100)
+        v (gen-velocity num -1 1)
+        p (gen-position num -100 100)
+        dt 0.01]
+    (permanent! m)
+    (prn-tex m) (prn-tex v) (prn-tex p)
+    (loop [v v, p p, i 0]
+      (if (> i iterations)
+        nil
+        (let [a (acquire! (chunked-add #(gravity {:g 6.673e-11 :idx %} [m p]) 10 num))
+              v (acquire! (add {:k dt} [v a]))
+              p (add {:k dt} [p v])]
+              ;mv (first (sum (kinetic-energy [m v])))
+              ;mgh (first (sum (chunked-add #(potential-energy {:idx %} [m p]) 10 num)))]
+          ;(println "k:" mv "p:" mgh "diff:" (- mv mgh))
+          (prn-tex a) (prn-tex v) (prn-tex p)
+          (recur v p (inc i)))))
+    (ephemeral! m)))
+
+(with-slate slate
+  (run-sim 5 10))
