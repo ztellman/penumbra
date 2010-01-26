bdeba089aefc398dda8e11afeb8ead5454703048
prospero <ztellman@gmail.com>
2010-01-25 19:19:35 -0800

    don't assume anti-aliasing is possible, and return to 1.1.0-new, since 1.2.0 master doesn't seem to work.  I'm looking into this.

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject penumbra "0.5.0-SNAPSHOT"
   :description "Penumbra is an idiomatic wrapper for OpenGL in Clojure, by way of LWJGL."
-  :dependencies [[org.clojure/clojure "1.2.0-master-SNAPSHOT"]
-                 [org.clojure/clojure-contrib "1.0-SNAPSHOT"]]
+  :dependencies [[org.clojure/clojure "1.1.0-new-SNAPSHOT"]
+                 [org.clojure/clojure-contrib "1.1.0"]]
   ;; TODO: native dependencies flag is needed here to support
   ;; downloading and extracting lwjgl with lein deps.
   ;; Should add as soon as native dependencies flag
