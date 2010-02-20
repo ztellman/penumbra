dfedcd12c737815720bc5185eba669f806652be3
prospero <ztellman@gmail.com>
2010-02-20 15:30:52 -0800

    update project.clj

--- project.clj
+++ project.clj
@@ -1,9 +1,8 @@
 (defproject penumbra "0.5.0-SNAPSHOT"
-  :description "Penumbra is an idiomatic wrapper for OpenGL in Clojure, by way of LWJGL."
-  :dependencies [[org.clojure/clojure "1.1.0-new-SNAPSHOT"]
-                 [org.clojure/clojure-contrib "1.1.0"]]
-  ;; TODO: native dependencies flag is needed here to support
-  ;; downloading and extracting lwjgl with lein deps.
-  ;; Should add as soon as native dependencies flag
-  ;; is implemented in leiningen.
-  :dev-dependencies [[leiningen/lein-swank "1.1.0"]])
+  :description "An idiomatic wrapper for OpenGL"
+  :dependencies [[org.clojure/clojure "1.1.0"]
+                 [org.clojure/clojure-contrib "1.1.0"]
+                 [slick-util "1.0.0"]]
+  :native-dependencies [[lwjgl "2.2.2"]]
+  :dev-dependencies [[native-deps "1.0.0"]
+                     [leiningen/lein-swank "1.1.0"]])
