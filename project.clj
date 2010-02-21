ef150b24fbbd5ab8cdd0da8d7b195d065ce20729
prospero <ztellman@gmail.com>
2010-02-20 18:46:35 -0800

    we're relying on leiningen now

--- project.clj
+++ project.clj
@@ -1,8 +1,9 @@
-(defproject penumbra "0.5.0-SNAPSHOT"
+(defproject penumbra "0.6.0-SNAPSHOT"
   :description "An idiomatic wrapper for OpenGL"
-  :dependencies [[org.clojure/clojure "1.1.0"]
-                 [org.clojure/clojure-contrib "1.1.0"]
+  :dependencies [[org.clojure/clojure "1.2.0-master"]
+                 [org.clojure/clojure-contrib "1.2.0-master"]
                  [slick-util "1.0.0"]]
   :native-dependencies [[lwjgl "2.2.2"]]
   :dev-dependencies [[native-deps "1.0.0"]
+                     [lein-clojars "0.5.0-SNAPSHOT"]
                      [leiningen/lein-swank "1.1.0"]])
