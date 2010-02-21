885db5d20693112470ba051c6546aaa42ccec49b
prospero <ztellman@gmail.com>
2010-02-21 10:28:29 -0800

    take clojure dependencies out of project.clj

--- project.clj
+++ project.clj
@@ -1,8 +1,6 @@
 (defproject penumbra "0.6.0-SNAPSHOT"
   :description "An idiomatic wrapper for OpenGL"
-  :dependencies [[org.clojure/clojure "1.2.0-master"]
-                 [org.clojure/clojure-contrib "1.2.0-master"]
-                 [slick-util "1.0.0"]]
+  :dependencies [[slick-util "1.0.0"]]
   :native-dependencies [[lwjgl "2.2.2"]]
   :dev-dependencies [[native-deps "1.0.0"]
                      [lein-clojars "0.5.0-SNAPSHOT"]
