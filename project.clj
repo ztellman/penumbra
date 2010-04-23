13203628dd35659c44f953c5981dff74caf05b8f
prospero <ztellman@gmail.com>
2010-04-23 01:57:47 -0700

    target LWJGL 2.4.2

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject penumbra "0.6.0-SNAPSHOT"
   :description "An idiomatic wrapper for OpenGL"
   :dependencies [[slick-util "1.0.0"]]
-  :native-dependencies [[lwjgl "2.2.2"]]
+  :native-dependencies [[penumbra/lwjgl "2.4.2"]]
   :dev-dependencies [[native-deps "1.0.0"]
                      [lein-clojars "0.5.0-SNAPSHOT"]
                      [leiningen/lein-swank "1.1.0"]])
