1f4c35167a610ccc8754f6d98c9114f31a5ae9aa
prospero <ztellman@gmail.com>
2010-04-24 23:11:45 -0700

    0.1.0-SNAPSHOT => 0.1.0

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject penumbra "0.6.0-SNAPSHOT"
   :description "An idiomatic wrapper for OpenGL"
   :dependencies [[slick-util "1.0.0"]
-                 [cantor "0.1.0-SNAPSHOT"]]
+                 [cantor "0.1.0"]]
   :native-dependencies [[penumbra/lwjgl "2.4.2"]]
   :dev-dependencies [[native-deps "1.0.0"]
                      [lein-clojars "0.5.0-SNAPSHOT"]
