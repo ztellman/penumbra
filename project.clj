67ffcf1d3b46e7fea34e5554ae5f7af8d26de98b
prospero <ztellman@gmail.com>
2010-05-30 14:15:24 -0700

    fix project.clj

--- project.clj
+++ project.clj
@@ -1,11 +1,7 @@
 (defproject penumbra "0.6.0-SNAPSHOT"
   :description "An idiomatic wrapper for OpenGL"
   :dependencies [[slick-util "1.0.0"]
-<<<<<<< HEAD:project.clj
                  [cantor "0.2.0-SNAPSHOT"]
-=======
-                 [cantor "0.1.0"]
->>>>>>> a5045ad609301cde724c2ca32b1596e1af5bff48:project.clj
                  [org.clojure/clojure "1.2.0-master-SNAPSHOT"] 
                  [org.clojure/clojure-contrib "1.2.0-SNAPSHOT"]]
   :native-dependencies [[penumbra/lwjgl "2.4.2"]]
