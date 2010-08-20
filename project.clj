706caf50e9cbd10df1640f22e3402e50911c6236
prospero <ztellman@gmail.com>
2010-08-19 21:06:00 -0700

    update targeted version to clojure 1.2.0

--- project.clj
+++ project.clj
@@ -2,10 +2,10 @@
   :description "An idiomatic wrapper for OpenGL"
   :dependencies [[slick-util "1.0.0"]
                  [cantor "0.2.1"]
-                 [org.clojure/clojure "1.2.0-master-SNAPSHOT"] 
-                 [org.clojure/clojure-contrib "1.2.0-SNAPSHOT"]]
+                 [org.clojure/clojure "1.2.0"] 
+                 [org.clojure/clojure-contrib "1.2.0"]]
   :native-dependencies [[penumbra/lwjgl "2.4.2"]]
   :dev-dependencies [[native-deps "1.0.1"]
-		     [autodoc "0.7.1"]
+                     [autodoc "0.7.1"]
                      [lein-clojars "0.5.0-SNAPSHOT"]
                      [leiningen/lein-swank "1.1.0"]])
